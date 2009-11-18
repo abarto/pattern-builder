f0fa2748abf0d84bac4b9dcab77272a91600ebfd
abarto <abarto@f95cda80-d3d1-11de-89a3-7d4a0ca7bf7e>
2009-11-18 19:16:17 +0000

    Added some missing methods and continued documenting the code.

--- src/com/googlecode/patternbuilder/CharacterClassBuilder.java
+++ src/com/googlecode/patternbuilder/CharacterClassBuilder.java
@@ -138,4 +138,12 @@ public class CharacterClassBuilder {
 	public String compile() {
 		return "[" + partialPattern.toString() + "]";
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CharacterClassBuilder [partialPattern=" + partialPattern + "]";
+	}
 }
